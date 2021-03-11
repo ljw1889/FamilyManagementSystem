70823c11f28c4fbe2452685c8366e7d401ebb69e
ljw1889 <ljw1889@gmail.com>
2021-03-12 01:21:45 +0900

    modify code

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -2,10 +2,10 @@
 public class MenuManager {
 
 	public static void main(String[] args) {
-		System.out.println("1.Add Family");
-		System.out.println("2.Delete Family");
-		System.out.println("3.Edit Family");
-		System.out.println("4.View Family");
-		System.out.println("5.Exit ");
+		System.out.println("1. Add Family");
+		System.out.println("2. Delete Family");
+		System.out.println("3. Edit Family");
+		System.out.println("4. View Family");
+		System.out.println("5. Exit ");
 		}
 }

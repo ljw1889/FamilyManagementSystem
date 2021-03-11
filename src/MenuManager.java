43043a46da385f302c6b8a81ab05fd5d12347f97
ljw1889 <ljw1889@gmail.com>
2021-03-12 01:17:47 +0900

    change code

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,7 +1,11 @@
 
-public class world {
+public class MenuManager {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
-	}
+		System.out.println("1.Add ");
+		System.out.println("2.Delete ");
+		System.out.println("3.Edit ");
+		System.out.println("4.View ");
+		System.out.println("5.Exit ");
+		}
 }

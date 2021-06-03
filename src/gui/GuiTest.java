2d71686d630e9df6504630793e121e64a477adb3
ljw1889 <ljw1889@gmail.com>
2021-06-03 14:53:52 +0900

    이제 미사용

---
+++ src/gui/GuiTest.java
@@ -0,0 +1,12 @@
+package gui;
+
+public class GuiTest {
+
+	public static void main(String[] args) {
+	//	WindowFrame frame = new WindowFrame();
+//		MenuSelection menuselection = new MenuSelection();
+//		FamilyAdder familyadder = new FamilyAdder();
+//		FamilyViewer familyviewer = new FamilyViewer();
+	}
+
+}
